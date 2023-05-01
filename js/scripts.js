a08d5a41a65a86abb93e477681fdb1fbf760df48
Brian Scherner <brianscherner@gmail.com>
2023-05-01 14:14:53 -0700

    Declare variables for each label input inside of form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 window.onload = function() {
 	let form = document.querySelector("form");
 	form.onsubmit = function(event) {
+		const person1Input = document.getElementById("person1Input").value;
+		const person2Input = document.getElementById("person1Input").value;
+		const animalInput = document.getElementById("animalInput").value;
+		const exclamationInput = document.getElementById("exclamationInput").value;
+		const verbInput = document.getElementById("verbInput").value;
+		const nounInput = document.getElementById("nounInput").value;
+
 		document.querySelector("span#person1a").innerText = "person 1";
 		document.querySelector("span#person1b").innerText = "person 1";
 		document.querySelector("span#person1c").innerText = "person 1";
